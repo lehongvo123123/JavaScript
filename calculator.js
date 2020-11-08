3458d332d1bd2ad34882c1fa927a8ce1750cf2a8
vo <volehe153209@fpt.edu.vn>
2020-11-08 08:18:09 +0700

    [Thực hành] Sử dụng toán tử

---
+++ calculator.js
@@ -0,0 +1,14 @@
+var inputWidth=prompt("Hãy nhập chiều dài a :")
+var inputHeight=prompt("Hãy nhập chiều dài b :")
+var a=parseInt(inputWidth);
+var b=parseInt(inputHeight);
+var c=a*b;
+document.write("Enter width: ") ;
+document.write("<br>") ;
+document.write(a);
+document.write("<br>");
+document.write("Enter height: ");
+document.write("<br>");
+document.write(b);
+document.write("<br>");
+document.write("The area is: "+c);
